048776c3982a8e3578253eeb94cb7512d75fc801
Diego Hamilton <diegohamilton26@gmail.com>
2020-06-28 06:26:11 -0300

    Organize code in functions
    This commit:
    - Organize code in functions
    - Update SPI to SPI2

--- main.c
+++ main.c
@@ -20,46 +20,123 @@
 #include "hal.h"
 #include "chprintf.h"
 #include "ff.h"
+#include "usrconf.h"
 
 
 /*===========================================================================*/
-/* FatFs related.                                                            */
+/* SD related.                                                            */
 /*===========================================================================*/
 
 /**
  * @brief FS object.
  */
 static FATFS SDC_FS;
+#define is_fs_ready(fs)  fs->fs_type
 
-/* FS mounted and ready.*/
-static bool fs_ready = FALSE;
+/* 
+ * MMC/SD over SPI driver configuration .
+ */
+static MMCConfig mmccfg = {SPI_DRIVER, &ls_spicfg, &hs_spicfg};
+MMCDriver MMCD1;
+void mmc_start_with_spi(MMCDriver *mmcd, MMCConfig *cfg);
+void spi_pin_config(void);
+
+/* 
+ * SD/FS related functions.
+ */
+BYTE sd_mount(FATFS *fs, MMCDriver *mmcd);
+static FRESULT scan_files(BaseSequentialStream *chp, char *path);
+
+/*===========================================================================*/
+/* Main and generic code.                                                    */
+/*===========================================================================*/
+
 
 /*
- * Maximum speed SPI configuration (18MHz, CPHA=0, CPOL=0, MSb first).
+ * Application entry point.
  */
-static const SPIConfig hs_spicfg = {
-  false,
-  NULL,
-  GPIOA,
-  4,
-  0,
-  0
-};
+int main(void) {
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
 
-/*
- * Low speed SPI configuration (281.250kHz, CPHA=0, CPOL=0, MSb first).
- */
-static const SPIConfig ls_spicfg = {
-  false,
-  NULL,
-  GPIOA,
-  4,
-  SPI_CR1_BR_2 | SPI_CR1_BR_1,
-  0
-};
+  /*
+   * Activates the serial driver 1 using the driver default configuration.
+   * PA9(TX) and PA10(RX) are routed to USART1.
+   */
+  sdStart(&SD2, NULL);
+  chprintf((BaseSequentialStream *)&SD2, "Ahoy, i'm alive \r\n");
 
-/* MMC/SD over SPI driver configuration.*/
-static MMCConfig mmccfg = {&SPID1, &ls_spicfg, &hs_spicfg};
+  mmc_start_with_spi(&MMCD1, &mmccfg);
+
+  /*
+   * On insertion SDC initialization and FS mount.
+   */
+  chprintf((BaseSequentialStream *)&SD2, "Trying to connect\r\n");
+  while (mmcConnect(&MMCD1)) {
+    chprintf((BaseSequentialStream *)&SD2, "Card not connected\r\n");
+    chThdSleepMilliseconds(200);
+  }
+  chprintf((BaseSequentialStream *)&SD2, "Connected?\r\n");
+
+  sd_mount(&SDC_FS, &MMCD1);
+
+  FRESULT err;
+
+  char b[100];
+  char c[12] = "/oi123.txt";
+  // c[0] = 0;
+  b[0] = 0;
+
+  FIL fp;
+  uint32_t bw;
+  err = f_open(&fp, c, FA_CREATE_NEW | FA_WRITE);
+  f_write(&fp, (void *)b, sizeof(b), (UINT *)&bw);
+  chprintf((BaseSequentialStream *)&SD2, "open = %d, written %d\r\n", err, bw);
+  f_close(&fp);
+
+  scan_files((BaseSequentialStream *)&SD2, (char *)&b);
+
+  while (true) {
+  }
+}
+
+void mmc_start_with_spi(MMCDriver *mmcd, MMCConfig *cfg) {
+  spi_pin_config();
+  mmcObjectInit(mmcd);
+  mmcStart(mmcd, cfg);
+}
+
+void spi_pin_config(void) {  
+  /*
+   * SPI I/O pins setup.
+   */
+  palSetPadMode(SPI_SCK_PORT, SPI_SCK_PIN, PAL_MODE_STM32_ALTERNATE_PUSHPULL);      // SCK
+  palSetPadMode(SPI_MISO_PORT, SPI_MISO_PIN, PAL_MODE_STM32_ALTERNATE_PUSHPULL);    // MISO
+  palSetPadMode(SPI_MOSI_PORT, SPI_MOSI_PIN, PAL_MODE_STM32_ALTERNATE_PUSHPULL);    // MOSI
+  palSetPadMode(SPI_CS_PORT, SPI_CS_PIN, PAL_MODE_OUTPUT_PUSHPULL);                 // CS
+  palSetPad(SPI_CS_PORT, SPI_CS_PIN);                                               // Enable CS
+}
+
+BYTE sd_mount(FATFS *fs, MMCDriver *mmcd) {
+  FRESULT err;
+
+  err = f_mount(fs, "/", 1);
+  if (err != FR_OK) {
+    chprintf((BaseSequentialStream *)&SD2, "Disconnecting\r\n");
+    mmcDisconnect(mmcd);
+  } else {
+    chprintf((BaseSequentialStream *)&SD2, "TO suave\r\n");
+  }
+
+  return is_fs_ready(fs);
+}
 
 static FRESULT scan_files(BaseSequentialStream *chp, char *path) {
   static FILINFO fno;
@@ -90,89 +167,4 @@ static FRESULT scan_files(BaseSequentialStream *chp, char *path) {
     }
   }
   return res;
-}
-
-/*===========================================================================*/
-/* Main and generic code.                                                    */
-/*===========================================================================*/
-
-MMCDriver MMCD1;
-
-/*
- * Application entry point.
- */
-int main(void) {
-  /*
-   * System initializations.
-   * - HAL initialization, this also initializes the configured device drivers
-   *   and performs the board-specific initializations.
-   * - Kernel initialization, the main() function becomes a thread and the
-   *   RTOS is active.
-   */
-  halInit();
-  chSysInit();
-
-  /*
-   * Activates the serial driver 1 using the driver default configuration.
-   * PA9(TX) and PA10(RX) are routed to USART1.
-   */
-  sdStart(&SD2, NULL);
-
-  /*
-   * SPI1 I/O pins setup.
-   */
-  palSetPadMode(IOPORT1, 5, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* SCK. */
-  palSetPadMode(IOPORT1, 6, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* MISO.*/
-  palSetPadMode(IOPORT1, 7, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* MOSI.*/
-  palSetPadMode(IOPORT1, 4, PAL_MODE_OUTPUT_PUSHPULL);
-  palSetPad(IOPORT1, 4);
-
-  chprintf((BaseSequentialStream *)&SD2, "Ahoy, i'm alive \r\n");
-
-  /*
-   * Initializes the MMC driver to work with SPI2.
-   */
-  palSetPadMode(IOPORT2, GPIOB_PIN12, PAL_MODE_OUTPUT_PUSHPULL);
-  palSetPad(IOPORT2, GPIOB_PIN12);
-  mmcObjectInit(&MMCD1);
-  mmcStart(&MMCD1, &mmccfg);
-
-  /*
-   * On insertion SDC initialization and FS mount.
-   */
-  chprintf((BaseSequentialStream *)&SD2, "Trying to connect\r\n");
-  while (mmcConnect(&MMCD1)) {
-    chprintf((BaseSequentialStream *)&SD2, "Card not connected\r\n");
-    chThdSleepMilliseconds(200);
-  }
-  chprintf((BaseSequentialStream *)&SD2, "Connected?\r\n");
-
-  
-  FRESULT err;
-
-  err = f_mount(&SDC_FS, "/", 1);
-  if (err != FR_OK) {
-    chprintf((BaseSequentialStream *)&SD2, "Disconnecting\r\n");
-    mmcDisconnect(&MMCD1);
-  } else {
-    chprintf((BaseSequentialStream *)&SD2, "TO suave\r\n");
-    fs_ready = TRUE;
-  }
-
-  char b[100];
-  char c[12] = "/oi123.txt";
-  // c[0] = 0;
-  b[0] = 0;
-
-  FIL fp;
-  uint32_t bw;
-  err = f_open(&fp, c, FA_CREATE_NEW | FA_WRITE);
-  f_write(&fp, (void *)b, sizeof(b), &bw);
-  chprintf((BaseSequentialStream *)&SD2, "open = %d, written %d\r\n", err, bw);
-  f_close(&fp);
-
-  scan_files((BaseSequentialStream *)&SD2, &b);
-
-  while (true) {
-  }
 }
